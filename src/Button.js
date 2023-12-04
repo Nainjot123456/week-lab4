48dbf5eacfe9cec3420597cadabec74c9e1b75df
Nainjot Singh <singhnainjot6@gmail.com>
2023-12-04 12:38:47 +0200

    week8-lab4  done

---
+++ src/Button.js
@@ -0,0 +1,15 @@
+// src/Button.js
+import React from 'react';
+
+export default function Button() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
+
